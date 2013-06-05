3b694a684011b966419ab1e7024beff3b7f23c9a
dfmadrid <david.fdmv@gmail.com>
2013-06-05 07:25:44 +0200

    added comment

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
+/*
+ * Doesn't follow the Heavy JS - Thin C/C++ Layer pattern to
+ * improve performance.
+ * TODO: instantiate a gmp big integer, a mpfr big fraction or
+ * a mpfr big floating point depending on user supplied input
+*/
+
 var bigInteger = require("./build/Release/bigInteger");
 
 module.exports = function(value, value2, value3){
